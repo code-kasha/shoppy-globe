11bf92f6e84174bd07ef992940a548e877a62ec8
Akash Damle <akash.damle@outlook.com>
2026-03-18 17:37:29 +0530

    0.11 - Fixes on Error Boundary, Routing and Lazy Routes

--- src/LazyRoute.jsx
+++ src/LazyRoute.jsx
@@ -2,8 +2,7 @@ import { Suspense } from "react"
 import Spinner from "./components/Spinner"
 import ErrorBoundary from "./components/ErrorBoundary"
 
-// eslint-disable-next-line no-unused-vars
-export default function LazyRoute({ Component }) {
+export default function LazyRoute({ component: Component }) {
 	return (
 		<ErrorBoundary>
 			<Suspense fallback={<Spinner message="Loading" />}>
